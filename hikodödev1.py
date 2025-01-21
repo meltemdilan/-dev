01aac5cb61c5946dc5ee67dc791b76ed6e800abb
meltemdilan <dilan2626@icloud.com>
2025-01-21 13:52:09 +0300

    Add files via upload

---
+++ hikodödev1.py
@@ -0,0 +1,48 @@
+"""#Ödev-1: Değişkenlere atanmış değerlerin veri tipleri arasında dönüşüm yapılır.
+x=3.9
+x=int(x)
+print(type(x))"""
+
+
+"""#Ödev-2: İsimlerden oluşan üç değişkene yaş değerleri atanır. Belirlenen üç değişken birbiriyle karşılaştırma operatörleri ile karşılaştırılır. Bu karşılaştırmalara mantıksal operatörler de eklenir.
+
+zeynep=8
+dilan=22
+betül=16
+print(bool(zeynep > dilan ))
+print(bool(betül > zeynep ))
+print(bool(zeynep < betül ))
+print(bool(dilan==zeynep))"""
+
+
+
+
+"""# Ödev-4: Kullanıcıdan isim, yaş, şehir ve meslek bilgilerini istenir ve cevaplarını yazdırılır.
+
+# Kullanıcıdan bilgileri al
+isim = input("İsminizi giriniz: ")
+yas = int(input("Yaşınızı giriniz: "))
+meslek = input("Mesleğinizi giriniz: ")
+sehir = input("Yaşadığınız şehri giriniz: ")
+
+# Bilgileri yazdır
+print("İsim:", isim)
+print("Yaş:", yas)
+print("Meslek:", meslek)
+print("Şehir:", sehir)"""
+
+
+
+"""#Ödev-5: "Hi-Kod Verı Bilimi Atölyesi" ifadesini bir değişkene tanımlanır.
+
+x = "Hi-Kod Veri Bilimi Atölyesi"
+print(x.split())
+print(x.upper())
+print(x.lower())"""
+
+#"0123456789" ifadesindeki yalnızca çift sayıları ve yalnızca tek sayıları seçilir. ("02468", "13579")
+
+"""for x in range(0, 10):
+    if x % 2 == 0:
+        print(x)"""
+
